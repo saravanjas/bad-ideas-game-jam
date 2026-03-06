4b4060f1aa163a20fbfe28fe9a94adb2b1bc167f
Jan <123597006+fistroni@users.noreply.github.com>
2026-03-06 19:34:12 +0100

    Dodani inputi

--- Modules/template1x1/template1x1.gd
+++ Modules/template1x1/template1x1.gd
@@ -1,6 +1,16 @@
 extends ModuleClass
 
+const 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	
+
+func actiavte():
+	var modules:Array[ModuleClass] = getAdjacentModules()
+	for module in modules:
+		if module.has_method("activate"):
+			module.activate()
