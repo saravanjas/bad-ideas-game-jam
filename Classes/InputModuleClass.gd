d2f11bab3ab35399a281d42738da3e0ceda227fa
Jan <123597006+fistroni@users.noreply.github.com>
2026-03-06 20:00:05 +0100

    Created InputModuleClass

--- Classes/InputModuleClass.gd
+++ Classes/InputModuleClass.gd
@@ -1,11 +1,7 @@
+class_name InputModuleClass
 extends ModuleClass
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.	
-
-func actiavte():
+func activate():
 	var modules:Array[ModuleClass] = getAdjacentModules()
 	for module in modules:
 		if module.has_method("activate"):
