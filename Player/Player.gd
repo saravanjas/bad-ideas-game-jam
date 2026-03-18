9df5273c21bcbe80c08f03c8a21e8f6d3af31211
Marko Grgic <sneakyturtle593@gmail.com>
2026-03-18 22:13:22 +0100

    Build Mode Visuals + Game Structure

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,16 @@
 extends ShipClass
 
 @onready var character_body_2d: CharacterBody2D = $CharacterBody2D
-@onready var tile_map_layer_body: TileMapLayer = $"CharacterBody2D/TileMapLayer"
+@onready var tile_map_layer_body: TileMapLayer = $CharacterBody2D/TileMapLayer
 @onready var root_center: Marker2D = $CharacterBody2D/RootCenter
 
 
 func _ready() -> void:
 	tilemap = tile_map_layer_body
+	
 	characterBody = character_body_2d
 	rootCenter = root_center
-	GlobalVariables.playerTilemap = tile_map_layer_body
-
+	GlobalVariables.playerTilemap = tilemap
 func _physics_process(_delta: float) -> void:
 	if GlobalVariables.gamePaused: return
 	
