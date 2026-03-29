5e49a32d35062b5cdcfdaf28fbf10cf18c58c222
saravanjas <166915565+saravanjas@users.noreply.github.com>
2026-03-29 01:08:43 +0100

    NAPOKON
    ne pitaj

--- Modules/template1x1/template1x1.gd
+++ Modules/template1x1/template1x1.gd
@@ -1,16 +1,10 @@
 extends ModuleClass
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var booster_timeout_node: Timer = $BoosterTimeout
-@onready var booster_sfx: AudioStreamPlayer2D = $boosterSFX
-
 var active : bool = false
+@onready var booster_timeout_node: Timer = $BoosterTimeout
 var current_anim : String 
 
-var boosterSoundPlaying := false
 func activate():
-	if !boosterSoundPlaying:
-		boosterSoundPlaying = true
-		booster_sfx.play()
 	animated_sprite_2d.visible = true
 	booster_timeout_node.start()
 	var vector:Vector2
@@ -38,5 +32,3 @@ func booster_timeout() -> void:
 	$AnimatedSprite2D.play("reset")
 	active = false
 	current_anim = "reset"
-	booster_sfx.stop()
-	boosterSoundPlaying = false
