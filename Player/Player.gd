1c3b1fff57bde41c5624a7a8aab522f51baf54fe
Jan <123597006+fistroni@users.noreply.github.com>
2026-03-30 21:19:32 +0200

    dodani restrictioni na buildmode

--- Player/Player.gd
+++ Player/Player.gd
@@ -26,5 +26,4 @@ func _physics_process(_delta: float) -> void:
 	
 	objective_marker.global_position = character_body_2d.global_position
 	var lookAngle = (GlobalVariables.nextObjective.global_position - character_body_2d.global_position).angle()
-	print(lookAngle)
 	objective_marker.rotation = lookAngle + PI/2
