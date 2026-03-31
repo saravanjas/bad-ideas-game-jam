0941ade7564128cd59bc49943315da073d833661
saravanjas <166915565+saravanjas@users.noreply.github.com>
2026-03-31 15:48:28 +0200

    Fixed Antennae Counter

--- Game/counter_display.gd
+++ Game/counter_display.gd
@@ -1,10 +1,5 @@
 extends Sprite2D
 
-var local_count = -1 
-
 func _process(_delta):
-	var current_global_count = GlobalVariables.anntenaeDestroyed
+	frame = int(GlobalVariables.anntenaeDestroyed)
 	
-	if current_global_count != local_count:
-		local_count = clamp(current_global_count, 0, 4)
-		frame = local_count
